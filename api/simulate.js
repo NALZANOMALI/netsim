e6778e992758bc0f7be869ed8076953dce2a254d
NALZANOMALI <rinalaz413@gmail.com>
2026-09-15 07:25:37 +0800

    Update simulate.js

--- api/simulate.js
+++ api/simulate.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -38,4 +40,4 @@ export default async function handler(req, res) {
     console.error('Proxy error:', err);
     res.status(500).json({ error: err.message });
   }
-}
+};
